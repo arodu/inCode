821b177f98d66061206710f08ea6496cab865bd3
Alberto Rodriguez <arod.unerg@gmail.com>
2015-08-01 12:12:52 -0430

    agregado algunos estilos y formateo de entrada de datos

--- js/inCode.js
+++ js/inCode.js
@@ -11,36 +11,80 @@ function inCode(editor, $consola, $btns, $options){
         }catch(err) {
             writeln(err.message, 'error');
         }
+        _debug("Ejecutar codigo: ");
     });
 
     function init(){
         var fecha = new Date();
         reset();
-        writeln(fecha.getTime(),'date');
+        writeln(fecha,'date');
+        _debug("Inicido consola");
     };
 
     function reset(){
         $consola.html("");
     };
 
-    function write(msg, type){
+    function write(msg, type, wDebug){
+        switch(type) {
+            case "error":
+                msg = '<span class="error"><strong>Error: </strong>'+msg+'</span>';
+                break;
+            case 'date':
+                msg = '<span class="date">'+msg+'</span>';
+                break;
+            case 'input_text':
+                msg = '<span class="input-text">'+msg+'</span>';
+                break;
+            case 'input_value':
+                msg = '<span class="input-value">'+msg+'</span>';
+                break;
+        }
         $consola.append(msg);
+        _debug("Escritura en consola: "+msg);
     };
 
     function writeln(msg, type){
-        write(msg + "<br/>");
+        write( msg+"<br/>", type );
+        //write();
     };
 
-    function read(msg, value){
-        return prompt(msg, value);
+    function read(msg, default_value){
+    	out = window.prompt(msg, default_value);
+        write(msg,"input_text");
+        writeln(out,"input_value");
+
+    	if (isFinite(out)) {
+    		n = Number(out);
+    		if(isInt(n)){
+    			n = parseInt(n);
+    		}else if(isFloat(n)){
+    			n = parseFloat(n);
+    		}
+    		out = n;
+    	}
+        _debug("Lectura de datos: "+out);
+        return out;
     };
 
-    function w_debug(msg){
+    function isInt(n){
+            return Number(n)===n && n%1===0;
+    };
+
+    function isFloat(n){
+            return   n===Number(n) && n%1!==0
+    };
+
+    function setDebug(value){
+        debug = value;
+    };
+
+    function _debug(msg){
         if(debug){
             console.log(msg);
         }
     }
 
-    w_debug("Iniciado objeto de tipo inCode");
+    _debug("Iniciado objeto de tipo inCode");
 
 }
